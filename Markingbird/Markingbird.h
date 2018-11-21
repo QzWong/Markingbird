133a697bc8aa14e18eb94c1ae98ec0374b6e6bad
Tri Vo <tri.vo@adurolife.com>
2018-11-21 09:21:36 +0700

    Fork

--- Markingbird/Markingbird.h
+++ Markingbird/Markingbird.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double MarkingbirdVersionNumber;
 FOUNDATION_EXPORT const unsigned char MarkingbirdVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Markingbird/PublicHeader.h>
-
-
